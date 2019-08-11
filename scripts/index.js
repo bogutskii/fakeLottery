11ff5cd17fa051c1350d8656c7ee4ca4424ceaaf
PBo <sl9site@gmail.com>
2019-08-11 14:11:48 -0700

    get all values from inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,18 @@
 function limit(elem) {
-  let max_chars = 2;
-  if(elem.value.length > max_chars) {
-    elem.value = elem.value.substr(0, max_chars);
-  }
+	let max_chars = 2;
+	if (elem.value.length > max_chars) {
+		elem.value = elem.value.substr(0, max_chars);
+	}
+}
+
+function getInputVal() {
+	const inputVal = document.getElementsByClassName('numbox');
+	let allInputValues = [];
+
+	for (let i = 0; i < inputVal.length; i++) {
+		allInputValues.push(inputVal[i].value);
+	}
+	let res = allInputValues.sort((a, b) => a - b);
+	console.log(res);
+	return res;
 }
