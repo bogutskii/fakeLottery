ccba0bca220c005bc972da706a6e070ff33049d7
PBo <sl9site@gmail.com>
2019-08-11 23:23:52 -0700

    match function (not ready)

--- scripts/index.js
+++ scripts/index.js
@@ -20,15 +20,42 @@ function limitSpecial(elem) {
 }
 
 // collects all input values into an array, and sorts (except for the special value at the end)
-function getInputVal() {
+const getInputVal = function getInputVal() {
 	const inputVal = document.getElementsByClassName('numbox');
 	let allInputValues = [];
 	for (let i = 0; i < inputVal.length; i++) {
 		allInputValues.push(inputVal[i].value);
 	}
 	let res = allInputValues.sort((a, b) => a - b);
-	  res.push(document.getElementsByClassName('specialnumbox')[0].value);
-	document.getElementById('userNumStatus').innerHTML = res.map(el => parseInt(el));
+	  res.push('*' + document.getElementsByClassName('specialNumBox')[0].value + '*');
+	document.getElementById('userNumStatus').innerHTML = res;
 	return res;
-}
+};
 
+//randomaizing win number
+const winningNumbers = function winNumbers() {
+	let win = [];
+	for (let i = 0; i < 20; i++) {
+		let rand1 = Math.floor(Math.random() * 69);
+		if (!win.includes(rand1)) {
+			win.push(rand1);
+		}
+	}
+	win.length = 5;
+	win.sort((a, b) => a - b).push('*' + Math.floor(Math.random() * 26) + '*');
+	document.getElementById('winNumStatus').innerHTML = win;
+	return win;
+};
+
+//Match numbers ---------need fix---------
+const matchNumbers = function matchNum(userNum,win) {
+	let arr = [];
+	for(let i = 0; i < win.length; i++){
+		let temp = win().shift();
+		if(userNum.includes(temp)){
+			arr.push(temp);
+		}
+	}
+	document.getElementById('matchNumStatus').innerHTML = arr;
+	return  arr;
+};
