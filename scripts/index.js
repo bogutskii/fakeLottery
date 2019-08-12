81ebcb6c549cb03d0556b886ab920acfde2b0a8a
PBo <sl9site@gmail.com>
2019-08-11 22:17:41 -0700

    special number limit

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,34 @@
+// limit for entering more than 69
 function limit(elem) {
 	let max_chars = 2;
-	if (elem.value.length > max_chars) {
+	if (elem.value > 69 ) {
+		elem.value = elem.value.substr(0, max_chars - 1);
+	}
+	else if (elem.value.length > max_chars) {
+		elem.value = elem.value.substr(0, max_chars);
+	}
+}
+// limit for special  entering more than 26
+function limitSpecial(elem) {
+	let max_chars = 2;
+	if (elem.value > 26 ) {
+		elem.value = elem.value.substr(0, max_chars - 1);
+	}
+	else if (elem.value.length > max_chars) {
 		elem.value = elem.value.substr(0, max_chars);
 	}
 }
 
+// collects all input values into an array, and sorts (except for the special value at the end)
 function getInputVal() {
 	const inputVal = document.getElementsByClassName('numbox');
 	let allInputValues = [];
-	  for (let i = 0; i < inputVal.length; i++) {
-	    //if(inputVal[i].value > 47)
-	    //return ('The number can`t be more than 47'); //  ned fix
+	for (let i = 0; i < inputVal.length; i++) {
 		allInputValues.push(inputVal[i].value);
 	}
 	let res = allInputValues.sort((a, b) => a - b);
-	  res.push(document.getElementsByClassName('specialnumbox')[0].value)
-  document.getElementById("numStatus").innerHTML = res;
-  return res;
+	  res.push(document.getElementsByClassName('specialnumbox')[0].value);
+	document.getElementById('userNumStatus').innerHTML = res.map(el => parseInt(el));
+	return res;
 }
+
